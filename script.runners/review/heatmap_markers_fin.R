7bc8715c43f568daa30b316f57479af5990caa04
Pam Dobay <maria.pamela.david@gmail.com>
2017-04-03 17:01:46 +0200

    Call to correct repository; test binarizeMarkers() function

--- script.runners/review/heatmap_markers_fin.R
+++ script.runners/review/heatmap_markers_fin.R
@@ -1,6 +1,6 @@
 library(gplots)
 
-getURL('https://raw.githubusercontent.com/pampernickel/subtyping/master/script.runners/routineFuncs.R', ssl.verifypeer = F) -> script
+getURL('https://raw.githubusercontent.com/pampernickel/subtyping/master/scripts/routineFuncs.r', ssl.verifypeer = F) -> script
 eval(parse(text=script))
 
 load("./r.data.files/second_proc/sub.list.fin_genes.rda") # limma_fin.r for processing
@@ -13,10 +13,10 @@ markers[-which(duplicated(markers$marker)),] -> markers
 # arrange df by group, and by markers
 orderBy(sub.list, "groups") -> sub.list
 sub.list$exprs.mat[which(rownames(sub.list$exprs.mat) %in% markers$marker),] -> df
-binarizeMarkers(df, sub.list$groups, sub.list$tenomic, markers, 0.7)
+binarizeMarkers(df, sub.list$groups, sub.list$tenomic, markers, 0.85)
 
 load("./r.data.files/external/sub.list.fin_genes.rda") # limma_fin.r for processing
 dropSamples(sub.list.batch1, which(sub.list.batch1$groups==0)) -> sub.list.batch1
 orderBy(sub.list.batch1, "groups") -> sub.list.batch1
 sub.list.batch1$exprs[which(rownames(sub.list.batch1$exprs) %in% markers$marker),] -> df
-binarizeMarkers(df, sub.list.batch1$groups, colnames(sub.list.batch1$exprs), markers, 0.8)
+binarizeMarkers(df, sub.list.batch1$groups, colnames(sub.list.batch1$exprs), markers, 0.85)
